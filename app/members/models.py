d6c298819dbaa5608458f79bef0daff815dab3e2
Jeon Young Hoon <hungyb0924@gmail.com>
2018-11-27 13:39:51 +0900

    User.nickname 필드 추가

--- app/members/models.py
+++ app/members/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class User(AbstractUser):
     img_profile = models.ImageField(upload_to='user', blank=True)
+    nickname = models.CharField(max_length=20, unique=True, blank=True, null=True)
