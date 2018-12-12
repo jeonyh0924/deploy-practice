cfa8c2a1c806853510e1407f04e542954493f9fd
hanoul <hanoul1124@gmail.com>
2018-12-12 14:24:59 +0900

    Movie Ticket Filter API View

--- app/members/admin.py
+++ app/members/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = (
+        'username',
+        'get_name',
+    )
 
-admin.site.register(User)
+    def get_name(self, obj):
+        return obj.last_name + obj.first_name
+
+admin.site.register(User, UserAdmin)
 
 # User admin page에 Reservation 을 Inline으로 추가할 것.
 # 빠른 예매 취소 등의 관리를 위한 페이지
