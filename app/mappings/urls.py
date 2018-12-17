704657dad4c17a38e1c9f6aa80bbb766bd970d2b
hanoul <hanoul1124@gmail.com>
2018-12-17 18:59:21 +0900

    App Reseravtion APIs

--- app/mappings/urls.py
+++ app/mappings/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import apis
 
 urlpatterns_api_movies = ([
-  path('list/', apis.MovieListView.as_view()),
+  path('', apis.MovieListView.as_view()),
   path('detail/<int:pk>/', apis.MovieDetailView.as_view()),
-  path('pre-movies/', apis.PreMovieListView.as_view()),
+  path('pre/', apis.PreMovieListView.as_view()),
 ], 'movies')
 
 urlpatterns_api_theaters = ([
-  path('list/', apis.TheaterListView.as_view()),
+  path('', apis.TheaterListView.as_view()),
   path('detail/<int:pk>/', apis.TheaterDetailView.as_view()),
 ], 'theaters')
--- app/reservations/urls.py
+++ app/reservations/urls.py
@@ -6,4 +6,8 @@ urlpatterns_api_tickets = ([
     path('filter/', apis.TicketFilteringView.as_view()),
     path('seats/<int:pk>/', apis.TicketSeatListView.as_view()),
     path('reservations/', apis.TicketReservationView.as_view()),
+    path('m/movies/', apis.AppTicketMovieListView.as_view()),
+    path('m/filter/<int:pk>/', apis.AppTicketFilteringView.as_view()),
+    path('m/seats/<int:pk>/', apis.TicketSeatListView.as_view()),
+    path('m/reservations/', apis.TicketReservationView.as_view()),
 ], 'tickets')
